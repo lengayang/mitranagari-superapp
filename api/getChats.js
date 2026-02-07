ef7998a096c1a8ad69cab2d8fb366dcd80657aad
lengayang <lengayang@gmail.com>
2026-02-07 22:58:16 +0700

    Update getChats.js

--- api/getChats.js
+++ api/getChats.js
@@ -1,5 +1,20 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    status: "API getChats hidup"
-  })
+export default async function handler(req, res) {
+  try {
+    const url = process.env.GAS_GET
+
+    if (!url) {
+      return res.status(500).json({
+        error: "GAS_GET belum di set di Vercel ENV"
+      })
+    }
+
+    const r = await fetch(url)
+    const data = await r.json()
+
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json({
+      error: err.toString()
+    })
+  }
 }
