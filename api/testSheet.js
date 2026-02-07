c2972f513cbe679be6be8ac082da3ae300c7e36b
lengayang <lengayang@gmail.com>
2026-02-07 23:44:25 +0700

    Update testSheet.js

--- api/testSheet.js
+++ api/testSheet.js
@@ -1,15 +1,28 @@
 export default async function handler(req, res) {
 
-  const r = await fetch(process.env.GAS_WEBHOOK,{
+  const url = process.env.GAS_WEBHOOK;
+
+  if(!url){
+    return res.send("GAS_WEBHOOK belum ada di ENV");
+  }
+
+  console.log("URL:",url);
+
+  const r = await fetch(url,{
     method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
     body: JSON.stringify({
       phone:"628111111111",
       name:"TEST",
       message:"TES MASUK",
       reply:""
     })
-  })
+  });
 
-  const text = await r.text()
-  res.send(text)
+  const text = await r.text();
+  console.log("RESP:",text);
+
+  res.send(text);
 }
